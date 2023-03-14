20de0efef7f36ca31528931ecb3e6cb89c8ed644
AStandish <112442942+AStandish@users.noreply.github.com>
2023-03-14 17:29:32 -0400

    updated chat

--- server/server.js
+++ server/server.js
@@ -68,5 +68,26 @@ io.on('connection', (socket) => {
   });
 });
 
+const NEW_CHAT_MESSAGE_EVENT = 'newChatMessage';
+
+io.on('connection', (socket) => {
+  console.log(`Client ${socket.id} connected`);
+
+  // Join a conversation
+  const { roomId } = socket.handshake.query;
+  socket.join(roomId);
+
+  // Listen for new messages
+  socket.on(NEW_CHAT_MESSAGE_EVENT, (data) => {
+    io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
+  });
+
+  // Leave the room if the user closes the socket
+  socket.on('disconnect', () => {
+    console.log(`Client ${socket.id} diconnected`);
+    socket.leave(roomId);
+  });
+});
+
 // Call the async function to start the server
 startApolloServer(typeDefs, resolvers);
