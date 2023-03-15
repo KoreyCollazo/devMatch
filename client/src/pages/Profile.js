72fa86d4ac0c0098951e338adf8e4535f2828688
Iuliia Lavine <iulii@MacBook-Pro.hitronhub.home>
2023-03-14 21:26:45 -0400

    undo commenting

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -10,43 +10,43 @@ import { QUERY_SINGLE_PROFILE, QUERY_ME } from '../utils/queries';
 import Auth from '../utils/auth';
 
 const Profile = () => {
-  // const { profileId } = useParams();
+  const { profileId } = useParams();
 
-  // // If there is no `profileId` in the URL as a parameter, execute the `QUERY_ME` query instead for the logged in user's information
-  // const { loading, data } = useQuery(profileId ? QUERY_SINGLE_PROFILE : QUERY_ME, {
-  //   variables: { profileId: profileId }
-  // });
+  // If there is no `profileId` in the URL as a parameter, execute the `QUERY_ME` query instead for the logged in user's information
+  const { loading, data } = useQuery(profileId ? QUERY_SINGLE_PROFILE : QUERY_ME, {
+    variables: { profileId: profileId }
+  });
 
-  // // Check if data is returning from the `QUERY_ME` query, then the `QUERY_SINGLE_PROFILE` query
-  // const profile = data?.me || data?.profile || {};
+  // Check if data is returning from the `QUERY_ME` query, then the `QUERY_SINGLE_PROFILE` query
+  const profile = data?.me || data?.profile || {};
 
-  // // Use React Router's `<Navigate />` component to redirect to personal profile page if username is yours
-  // if (Auth.loggedIn() && Auth.getProfile().data._id === profileId) {
-  //   return <Navigate to="/me" />;
-  // }
+  // Use React Router's `<Navigate />` component to redirect to personal profile page if username is yours
+  if (Auth.loggedIn() && Auth.getProfile().data._id === profileId) {
+    return <Navigate to="/me" />;
+  }
 
-  // if (loading) {
-  //   return <div>Loading...</div>;
-  // }
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-  // if (!profile?.name) {
-  //   return (
-  //     <div classNameName="row">
-  //       <div classNameName="col s2 m12">
-  //         <div classNameName="card">
-  //           <div classNameName="card-content">
-  //             <h4>
-  //               You need to be logged in to see your profile page.
-  //             </h4>
-  //           </div>
-  //           <div classNameName="card-action">
-  //             <Link id="login"  to={`/login`}>Login</Link>
-  //           </div>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   );
-  // }
+  if (!profile?.name) {
+    return (
+      <div classNameName="row">
+        <div classNameName="col s2 m12">
+          <div classNameName="card">
+            <div classNameName="card-content">
+              <h4>
+                You need to be logged in to see your profile page.
+              </h4>
+            </div>
+            <div classNameName="card-action">
+              <Link id="login"  to={`/login`}>Login</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="row">
