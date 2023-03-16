d3e36ce466d51ffd6cf5a1e927fdaccb8ef99abb
AStandish <112442942+AStandish@users.noreply.github.com>
2023-03-16 19:55:29 -0400

    added changes to files

---
+++ client/src/components/Questionnaire/questionnaire.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Question from '../components/Question';
+import QuestionCount from '../components/QuestionCount';
+import AnswerOption from '../components/AnswerOption';
+
+function Questionnaire(props) {
+  function renderAnswerOptions(key) {
+    return (
+      <AnswerOption
+        key={key.content}
+        answerContent={key.content}
+        answerType={key.type}
+        answer={props.answer}
+        questionId={props.questionId}
+        onAnswerSelected={props.onAnswerSelected}
+      />
+    );
+  }
+
+  return (
+      <div key={props.questionId}>
+        <QuestionCount counter={props.questionId} total={props.questionTotal} />
+        <Question content={props.question} />
+        <ul className="answerOptions">
+          {props.answerOptions.map(renderAnswerOptions)}
+        </ul>
+      </div>
+  );
+}
+
+Questionnaire.propTypes = {
+  answer: PropTypes.string.isRequired,
+  answerOptions: PropTypes.array.isRequired,
+  question: PropTypes.string.isRequired,
+  questionId: PropTypes.number.isRequired,
+  questionTotal: PropTypes.number.isRequired,
+  onAnswerSelected: PropTypes.func.isRequired
+};
+
+export default Questionnaire;
