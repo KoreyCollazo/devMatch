e39bfed6276cf4e1d7ba814f6f92a37f6c9b906a
ezmaa <ezinmark@gmail.com>
2023-03-17 13:03:36 -0400

    working on query where specific fields are coming back with null data, routed button to form page after sign in

--- server/models/Message.js
+++ server/models/Message.js
@@ -1,7 +1,6 @@
 // const { Schema, model } = require('mongoose');
 // const dateFormat = require('../utils/dateFormat');
 
-<<<<<<< HEAD
 // const messageSchema = new Schema({
 //   message: {
 //     unique: true,
@@ -21,31 +20,7 @@
 //     get: (timestamp) => dateFormat(timestamp)
 //   }
 // });
-=======
-const messageSchema = new Schema({
-  message: {
-    type: String,
-    minlength: 2,
-    maxlength: 280,
-    trim: true
-  },
-  messageAuthor: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (timestamp) => dateFormat(timestamp)
-  }
-});
->>>>>>> origin/main
 
 // const Message = model('Message', messageSchema);
 
-<<<<<<< HEAD
 // module.exports = Message;
-=======
-module.exports = Message;
->>>>>>> origin/main
