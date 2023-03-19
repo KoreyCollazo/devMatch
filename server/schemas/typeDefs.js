7ced24026a08b1664fc699436057986535548ba0
ezmaa <ezinmark@gmail.com>
2023-03-19 09:57:27 -0400

    fixed syntax errors and names added getMatches to schema, working app

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,7 +2,6 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type User {
-  type User {
     _id: ID
     firstName: String
     lastName: String
@@ -35,6 +34,7 @@ const typeDefs = gql`
   type Query {
     users: [User]!
     user(UserId: ID!): User
+    getMatches: [User]
     # Because we have the context functionality in place to check a JWT and decode its data, we can use a query that will always find and return the logged in user's data
     me: User
   }
