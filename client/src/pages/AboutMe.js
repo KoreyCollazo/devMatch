69a3f65b9cc6a396c8141c7f035c3d52ebb62929
Iuliia Lavine <iulii@MacBook-Pro.hitronhub.home>
2023-03-21 12:13:24 -0400

    fix sign up styling

--- client/src/pages/AboutMe.js
+++ client/src/pages/AboutMe.js
@@ -84,150 +84,115 @@ const AboutMe = () => {
                 <div>
                   <div className="row">
                     <form className="col s12" onSubmit={handleFormSubmit}>
-                      <div className="row">
-                        <div class="input-field col s6">
-                          <input
-                            className="form-input"
-                            placeholder="Your email"
-                            name="email"
-                            type="email"
-                            value={formState.email}
-                            onChange={handleChange}
-                          />
-                        </div>
-
-                        <div className="input-field col s6">
-                          <input
-                            className="form-input"
-                            placeholder="******"
-                            name="password"
-                            type="password"
-                            value={formState.password}
-                            onChange={handleChange}
-                          />
-                        </div>
-                      </div>
-                      <div>
-                        <div class="input-field col s6">
-                          <input
-                            type="text"
-                            id="autocomplete-input"
-                            class="autocomplete"
-                            value={formState.location}
-                            onChange={handleChange}
-                          />
-                          <label for="autocomplete-input">Enter your First Name</label>
-                        </div>
-                        {/* <div class="input-field col s6">
-                        <input
-                          className="form-input"
-                          placeholder="Your last name"
-                         
-                          name="lastName"
-                          type="text"
-                          value={formState.lastName}
-                          onChange={handleChange}
-                        />
-                      </div> */}
-                        <div class="input-field col s6">
-                          <input
-                            type="text"
-                            id="autocomplete-input"
-                            class="autocomplete"
-                            value={formState.location}
-                            onChange={handleChange}
-                          />
-                          <label for="autocomplete-input">Enter your last name</label>
-                        </div>
-                      </div>
-                      <div className="row">
-                        <div className="input-field col s6">
-                          <DatePicker
-                            selected={dateValue}
-                            onChange={(date) => setDateValue(date)}
-                          />
-                          <span className="helper-text" data-error="wrong" data-success="right">
-                            DOB
-                          </span>
-                        </div>
-
-                        <div className="input-field col s6">
-                          <Select options={optionsGender} />
-
-                          <span
-                            name="gender"
-                            className="helper-text"
-                            data-error="wrong"
-                            data-success="right"
-                            type="select"
-                            value={formState.gender}
-                            onChange={handleChange}>
-                            Select your gender
-                          </span>
-                        </div>
-                      </div>
-                      <div className="row">
-                        <div className="input-field col s6">
-                          <Select options={optionsEdu} />
-
-                          <span
-                            name="education"
-                            className="helper-text"
-                            data-error="wrong"
-                            data-success="right"
-                            value={formState.education}
-                            onChange={handleChange}>
-                            Select your education
-                          </span>
-                        </div>
-                        <div className="input-field col s6">
-                          <input
-                            type="text"
-                            id="autocomplete-input"
-                            className="autocomplete"
-                            value={formState.location}
-                            onChange={handleChange}
-                          />
-                          <label for="autocomplete-input">Enter your location</label>
-                        </div>
-                      </div>
-                      <div className="row">
-                        <div className="input-field col s6">
-                          <input
-                            type="text"
-                            id="autocomplete-input"
-                            className="autocomplete"
-                            value={formState.height}
-                            onChange={handleChange}
-                          />
-                          <label for="autocomplete-input">Enter your height</label>
-                        </div>
-                        <div className="input-field col s6">
-                          <input
-                            type="text"
-                            id="autocomplete-input"
-                            className="autocomplete"
-                            value={formState.ethnicity}
-                            onChange={handleChange}
-                          />
-                          <label for="autocomplete-input">Enter your ethnicity</label>
-                        </div>
-                      </div>
-                      <div className="row">
-                        <div className="row">
-                          <form className="col s12">
-                            <div className="row">
-                              <div className="input-field col s12">
-                                <textarea
-                                  id="textarea2"
-                                  className="materialize-textarea"
-                                  data-length="120"></textarea>
-                                <label for="autocomplete-input">Bio</label>
+                      <div class="container">
+                        <div class="row">
+                          <form class="col s12" id="reg-form">
+                            <div class="row">
+                              <div class="input-field col s6">
+                                <input id="first_name" type="text" class="validate" required />
+                                <label for="first_name">First Name</label>
+                              </div>
+                              <div class="input-field col s6">
+                                <input id="last_name" type="text" class="validate" required />
+                                <label for="last_name">Last Name</label>
                               </div>
                             </div>
+                            <div class="row">
+                              <div class="input-field col s12">
+                                <input id="email" type="email" class="validate" required/>
+                                <label for="email">Email</label>
+                              </div>
+                            </div>
+                            <div class="row">
+                              <div class="input-field col s12">
+                                <input
+                                  id="password"
+                                  type="password"
+                                  class="validate"
+                                  minlength="6"
+                                  required
+                              
+                                />
+                                <label for="password">Password</label>
+                              </div>
+                            </div>
+                      
                           </form>
                         </div>
+                
                       </div>
+
+                     
+
+                      <DatePicker selected={dateValue} onChange={(date) => setDateValue(date)} />
+                      <span className="helper-text" data-error="wrong" data-success="right">
+                        DOB
+                      </span>
+
+                      <Select options={optionsGender} />
+
+                      <span
+                        name="gender"
+                        className="helper-text"
+                        data-error="wrong"
+                        data-success="right"
+                        type="select"
+                        value={formState.gender}
+                        onChange={handleChange}>
+                        Select your gender
+                      </span>
+
+                      <Select options={optionsEdu} />
+
+                      <span
+                        name="education"
+                        className="helper-text"
+                        data-error="wrong"
+                        data-success="right"
+                        value={formState.education}
+                        onChange={handleChange}>
+                        Select your education
+                      </span>
+
+                      <input
+                        type="text"
+                        id="autocomplete-input"
+                        className="autocomplete"
+                        value={formState.location}
+                        onChange={handleChange}
+                      />
+                      <label for="autocomplete-input">Enter your location</label>
+
+                      <input
+                        type="text"
+                        id="autocomplete-input"
+                        className="autocomplete"
+                        value={formState.height}
+                        onChange={handleChange}
+                      />
+                      <label for="autocomplete-input">Enter your height</label>
+
+                      <input
+                        type="text"
+                        id="autocomplete-input"
+                        className="autocomplete"
+                        value={formState.ethnicity}
+                        onChange={handleChange}
+                      />
+                      <label for="autocomplete-input">Enter your ethnicity</label>
+
+                      <form className="col s12">
+                        <div className="row">
+                          <div className="input-field col s12">
+                            <textarea
+                              id="textarea2"
+                              className="materialize-textarea"
+                              data-length="120"></textarea>
+                            <label for="autocomplete-input">Bio</label>
+                          </div>
+                        </div>
+                      </form>
+
                       <button
                         className="btn btn-block btn-info"
                         style={{ cursor: 'pointer' }}
